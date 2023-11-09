d4ea7bf7b3fe87e0dbad71fb2b2e6b3ab039f400
Do <anthodo@deloitte.com>
2023-11-09 15:46:50 -0700

    update code

--- app.py
+++ app.py
@@ -2,25 +2,25 @@ from distutils.log import debug
 from fileinput import filename 
 from flask import *  
 import pdf_reader
-from pdf_reader import load_db
+from pdf_reader import load_db, load_db_sum
+from langchain.memory import ConversationBufferMemory
 
 
 
-app = Flask(__name__)   
+app = Flask(__name__, static_folder='static')   
+from flask_session import Session
+ 
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
-# Define calculator
+# Define bot
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/upload',methods=['POST'])
-# def predict():
-#     loaded_file = "./docs/Anthony_ResumeATS.pdf"
-#     chat = load_db(loaded_file,"stuff", 4)
-#     user_input = request.form['user_input']
-#     result = chat({'question': user_input})
-
-#     return render_template('index.html', prediction_text=str(result['answer']))  
         
 def success():
     if request.method == 'POST':
@@ -29,10 +29,22 @@ def success():
         # Check if the file has a ".pdf" file extension
         if f.filename.endswith(".pdf"):
             f.filename = "upload.pdf"
+            global file
+            file = f.filename
             # Use the custom path along with the original file name
             f.save(f.filename)
+            global chat
+            chat = load_db(file)
+            summ = load_db_sum(file)
+            global history
+            history = {}
+            conversation = {}
+            global summary
+            summary_result = summ({"question": "Can you summarize in detail?"})
+            summary = str(summary_result['answer'])
+
             
-            return render_template("chatbot.html", name=f.filename, history=history, conversation=conversation)
+            return render_template("chatbot.html", name=f.filename, history=history, conversation=conversation, summary=summary)
         else:
             return render_template("index.html", error_text="Only PDF files are allowed for upload.")
 
@@ -41,19 +53,17 @@ conversation = {}
 @app.route('/chatbot', methods=['GET', 'POST'])  
 
 def pdf():
-    loaded_file = 'upload.pdf'
-    chat = load_db(loaded_file,"stuff", 4)
     user_input = request.form['user_input']
     result = chat({"question": user_input})
     answer_text = str(result['answer'])
     question_text = str(result['question'])
     user = "User: "
-    chat = "Chat Bot: "
-    conversation.update({user: question_text, chat: answer_text})
+    chatbot = "Chat Bot: "
+    conversation.update({user: question_text, chatbot: answer_text})
     history.update({question_text : answer_text})
-    return render_template('chatbot.html', answer_text=answer_text, question_text=question_text, history = history, conversation = conversation)
-    return render_template('chatbot.html')
-  
+    return render_template('chatbot.html', answer_text=answer_text, question_text=question_text, history = history, conversation = conversation, summary = summary)
+    
+
 if __name__ == '__main__':   
     app.run(debug=True)
 
--- pdf_reader.py
+++ pdf_reader.py
@@ -8,6 +8,8 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import TextLoader
 from langchain.document_loaders import PyPDFLoader
+from langchain.vectorstores import Chroma
+from langchain.prompts import PromptTemplate
 import panel as pn
 import param
 
@@ -31,7 +33,7 @@ if current_date < datetime.date(2023, 9, 2):
 else:
     llm_name = "gpt-3.5-turbo"
 
-def load_db(file, chain_type, k):
+def load_db(file):
     # load documents
     loader = PyPDFLoader(file)
     documents = loader.load()
@@ -42,18 +44,81 @@ def load_db(file, chain_type, k):
     embeddings = OpenAIEmbeddings()
     # create vector database from data
     db = DocArrayInMemorySearch.from_documents(docs, embeddings)
+    # retriever = db.as_retriever()
+    
+    prompt_template_doc = """
+
+    Use the following pieces of context to answer the question at the end. {context}
+    You can also look into chat history. {chat_history}
+    If you still can't find the answer, please respond: "Please ask a question related to the document."
+
+    Question: {question}
+    Answer:
+    """
+    prompt_doc = PromptTemplate(
+        template=prompt_template_doc,
+        input_variables=["context", "question", "chat_history"],
+    )
     # define retriever
-    retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": k})
+    retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 4})
     #Keeps a buffer of history and process it
     memory = ConversationBufferMemory(
         memory_key="chat_history",
+        output_key="answer",
         return_messages=True
     )
     # create a chatbot chain. Memory is managed externally.
     qa = ConversationalRetrievalChain.from_llm(
         llm=ChatOpenAI(model_name=llm_name, temperature=0), 
-        chain_type=chain_type, 
+        chain_type="stuff", 
         retriever=retriever,
+        combine_docs_chain_kwargs={"prompt": prompt_doc},
+        memory=memory
+    )
+    return qa 
+
+def load_db_sum(file):
+    # load documents
+    loader = PyPDFLoader(file)
+    documents = loader.load()
+    # split documents
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+    docs = text_splitter.split_documents(documents)
+    # define embedding
+    embeddings = OpenAIEmbeddings()
+    # create vector database from data
+    db = DocArrayInMemorySearch.from_documents(docs, embeddings)
+    # retriever = db.as_retriever()
+    
+    # prompt_template_doc = """
+    # Use chat history : {chat_history} to determine the condition you are to research if not blank
+
+    # Use the following pieces of context to answer the question at the end.
+    # {context}
+    # If you still cant find the answer, just say that you don't know and don't try to make up an answer.
+    # You can also look into chat history.
+    # {chat_history}
+    # Question: {question}
+    # Answer:
+    # """
+    # prompt_doc = PromptTemplate(
+    #     template=prompt_template_doc,
+    #     input_variables=["context", "question", "chat_history"],
+    # )
+    # define retriever
+    retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 4})
+    #Keeps a buffer of history and process it
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        output_key="answer",
+        return_messages=True
+    )
+    # create a chatbot chain. Memory is managed externally.
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=ChatOpenAI(model_name=llm_name, temperature=0), 
+        chain_type="stuff", 
+        retriever=retriever,
+        # combine_docs_chain_kwargs={"prompt": prompt_doc},
         memory=memory
     )
     return qa 
